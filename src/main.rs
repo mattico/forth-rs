dcb4109157232467014fd80185a2d38b720ae17b
Matt Ickstadt <matthew.ickstadt@gmail.com>
2015-05-20 23:38:28 -0500

    Change number type to i32 for now. Add OVER, ROT, MOD, AND, OR.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(float_from_str_radix, convert, collections)]
+#![feature(convert, collections)]
 #![allow(unused_variables)]
 
 use std::collections::HashMap;
@@ -6,14 +6,15 @@ use std::hash::{Hash, Hasher};
 use std::io;
 use std::io::prelude::*;
 use std::rc::Rc;
+use std::fmt;
 
 type Dictionary = HashMap<String, Rc<Entry>>;
 type Statement = Vec<Op>;
 type NativeFn = Box<Fn(&mut Vec<Op>, &mut Interpreter)>;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Op {
-	Number(f64),
+	Number(i32),
 	Word(Rc<Entry>),
 }
 
@@ -40,10 +41,16 @@ impl Hash for Entry {
 	}
 }
 
+impl fmt::Debug for Entry {
+	fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		self.name.fmt(fmt)
+	}
+}
+
 impl Entry {
-	fn new(name: String, code: Code) -> Entry {
+	fn new(name: &str, code: Code) -> Entry {
 		Entry {
-			name: name,
+			name: String::from_str(name),
 			code: Rc::new(code),
 		}
 	}
@@ -70,32 +77,39 @@ impl Code {
 }
 
 trait StatementExt {
-	fn pop_number(&mut self, &mut Interpreter) -> f64;
-	fn run(&mut self, &mut Interpreter) -> f64;
+	fn pop_number(&mut self, &mut Interpreter) -> i32;
+	fn run(&mut self, &mut Interpreter) -> Result<i32, Statement>;
 }
 
 impl StatementExt for Statement {
-	fn pop_number(&mut self, interp: &mut Interpreter) -> f64 {
+	fn pop_number(&mut self, interp: &mut Interpreter) -> i32 {
 		loop {
 			match self.pop() {
 				Some(Op::Number(n)) => { return n; },
 				Some(Op::Word(s)) => s.code.run(self, interp),
-				None => { panic!("Popped empty statement"); },
+				None => panic!("Popped empty statement"),
 			}
 		}
 	}
-	fn run(&mut self, interp: &mut Interpreter) -> f64 {
+	fn run(&mut self, interp: &mut Interpreter) -> Result<i32, Statement> {
 		while self.len() > 0 {
 			match self.pop() {
-				Some(Op::Number(n)) => { return n },
+				Some(Op::Number(n)) => {
+					if self.len() == 0 {
+						return Ok(n)
+					} else {
+						self.push(Op::Number(n));
+						return Err(self.clone())
+					}
+				},
 				Some(Op::Word(w)) => {
 					let code = interp.dictionary[w.name.as_str()].code.clone();
 					code.run(self, interp);
 				},
-				None => { panic!("Empty Statement") },
+				None => unreachable!(),
 			};
 		}
-		unreachable!();
+		Ok(0)
 	}
 }
 
@@ -115,8 +129,7 @@ struct Interpreter {
 
 macro_rules! binary_entry {
 	($name:expr, $o:expr) => {
-		Entry::new(
-				String::from_str($name),
+		Entry::new($name,
 				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
 					let b = stack.pop_number(interp);
 					let a = stack.pop_number(interp);
@@ -128,8 +141,7 @@ macro_rules! binary_entry {
 
 macro_rules! unary_entry {
 	($name:expr, $o:expr) => {
-		Entry::new(
-				String::from_str($name),
+		Entry::new($name,
 				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
 					let b = stack.pop_number(interp);
 					stack.push(Op::Number($o(a)));
@@ -140,17 +152,13 @@ macro_rules! unary_entry {
 
 macro_rules! nonary_entry {
 	($name:expr, $o:expr) => {
-		Entry::new(
-				String::from_str($name),
+		Entry::new($name,
 				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
 					Op::Number($o);
 				})),
 			)
 	}
 }
-
-
-
 
 impl Interpreter {
 	fn new() -> Interpreter {
@@ -160,17 +168,39 @@ impl Interpreter {
 		dict.insert_entry(binary_entry!("*", ::std::ops::Mul::mul));
 		dict.insert_entry(binary_entry!("+", ::std::ops::Add::add));
 		dict.insert_entry(binary_entry!("-", ::std::ops::Sub::sub));
-		dict.insert_entry(binary_entry!("%", ::std::ops::Rem::rem));
+		dict.insert_entry(binary_entry!("MOD", ::std::ops::Rem::rem));
+		dict.insert_entry(binary_entry!("AND", ::std::ops::BitAnd::bitand));
+		dict.insert_entry(binary_entry!("OR", ::std::ops::BitOr::bitor));
 
 		dict.insert_entry(nonary_entry!("QUIT", ::std::process::exit(0)));
 		dict.insert_entry(nonary_entry!("bye", ::std::process::exit(0)));
 
-		dict.insert_entry(Entry::new(
-				String::from_str("DUP"),
+		dict.insert_entry(Entry::new("DUP",
 				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
 					let x = stack.pop().unwrap();
 					stack.push(x.clone());
 					stack.push(x);
+				})),
+			));
+
+		dict.insert_entry(Entry::new("OVER",
+				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
+					let x = stack.pop().unwrap();
+					let y = stack.pop().unwrap();
+					stack.push(x.clone());
+					stack.push(y);
+					stack.push(x);
+				})),
+			));
+
+		dict.insert_entry(Entry::new("ROT",
+				Code::Native(Box::new(|stack: &mut Statement, interp: &mut Interpreter| {
+					let x = stack.pop().unwrap();
+					let y = stack.pop().unwrap();
+					let z = stack.pop().unwrap();
+					stack.push(y);
+					stack.push(x);
+					stack.push(z);
 				})),
 			));
 
@@ -179,11 +209,11 @@ impl Interpreter {
 		}
 	}
 
-	fn lex(&mut self, statement: String) -> Statement {
+	fn lex(&mut self, statement: &str) -> Statement {
 		let words = statement.split(' ');
 		let mut ret = Statement::new();
 		for word in words {
-			if let Some(n) = f64::from_str_radix(word, 10).ok() {
+			if let Some(n) = i32::from_str_radix(word, 10).ok() {
 				ret.push(Op::Number(n));
 			} else {
 				ret.push(Op::Word(self.dictionary[word].clone()));
@@ -192,7 +222,7 @@ impl Interpreter {
 		ret
 	}
 
-	fn exec(&mut self, statement: String) -> f64 {
+	fn exec(&mut self, statement: &str) -> Result<i32, Statement> {
 		let mut statement = self.lex(statement);
 		return statement.run(self);
 	}
@@ -208,7 +238,13 @@ fn main() {
     	io::stdout().flush().ok().expect("Could not flush stdout");
     	let mut line = String::new();
     	stdin.read_line(&mut line).ok().expect("Unable to read from stdin");
-    	let result = interp.exec(String::from_str(line.trim_right()));
-    	println!("{}", result);
+    	match interp.exec(line.trim_right()) {
+    		Err(s) => {
+    			println!("{:?}", s);
+    		},
+    		Ok(n) => {
+    			println!("{}", n);
+    		},
+    	}
     }
 }
