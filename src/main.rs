49b96468e88dafed0e6f028ae0d7061e7a628299
Matt Ickstadt <matthew.ickstadt@gmail.com>
2015-05-29 21:31:33 -0500

    Remove 'Forth>' prompt and fix indentation in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,17 @@ extern crate forth;
 use forth::interpreter::Interpreter;
 
 fn main() {
-	println!("forth-rs version 0.1.0");
-	println!("Type 'bye' to exit");
+    println!("forth-rs version 0.1.0");
+    println!("Type 'bye' to exit");
     let mut interp = Interpreter::new();
     let mut stdin = io::stdin();
     loop {
-    	print!("Forth> ");
-    	io::stdout().flush().ok().expect("Could not flush stdout");
-    	let mut line = String::new();
-    	stdin.read_line(&mut line).ok().expect("Unable to read from stdin");
-    	match interp.exec(line.trim()) {
-    		Err(e) => println!("{:?}", e),
-    		Ok(_) => {},
-    	}
+        io::stdout().flush().ok().expect("Could not flush stdout");
+        let mut line = String::new();
+        stdin.read_line(&mut line).ok().expect("Unable to read from stdin");
+        match interp.exec(line.trim()) {
+            Err(e) => println!("{:?}", e),
+            Ok(_) => {},
+        }
     }
 }
